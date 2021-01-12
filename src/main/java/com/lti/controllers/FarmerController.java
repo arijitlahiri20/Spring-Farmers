ab2d69a17427a49a415c5a8fdfdb5ceecafdd53f
Sandy-112hey <sandesh.parekh@lntinfotech.com>
2021-01-12 16:47:31 +0530

    sell_request done

--- src/main/java/com/lti/controllers/FarmerController.java
+++ src/main/java/com/lti/controllers/FarmerController.java
@@ -1,5 +1,40 @@
 package com.lti.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.lti.dto.RegisterStatus;
+import com.lti.dto.Status;
+import com.lti.dto.Status.StatusType;
+import com.lti.entities.SellRequests;
+import com.lti.services.FarmerService;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:4200")
 public class FarmerController {
 
+	@Autowired
+	private FarmerService farmerService;
+	
+	@PostMapping("/farmer/place-sell-request")
+	public @ResponseBody Status placeSellRequest(@RequestBody SellRequests sell) {
+		try {
+			int id = farmerService.placeSellRequest(sell);
+			RegisterStatus status = new RegisterStatus();
+			status.setStatus(StatusType.SUCCESS);
+			status.setMessage("Crop Sell Request has been placed.");
+			status.setRegisteredCustomerId(id);
+			return status;
+		}
+		catch(Exception e) {
+			RegisterStatus status = new RegisterStatus();
+			status.setStatus(StatusType.FAILED);
+			status.setMessage(e.getMessage());
+			return status;
+		}
+	}
 }
--- src/main/java/com/lti/services/FarmerService.java
+++ src/main/java/com/lti/services/FarmerService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.lti.daos.FarmerDAO;
+import com.lti.entities.SellRequests;
 
 @Service 
 @Transactional
@@ -13,4 +14,9 @@ public class FarmerService implements IFarmerService{
 
 	@Autowired
 	private FarmerDAO farmerDAO;
+
+	public int placeSellRequest(SellRequests sell) {
+		SellRequests sr = (SellRequests) farmerDAO.save(sell);
+		return sr.getUser_id();
+	}
 }
