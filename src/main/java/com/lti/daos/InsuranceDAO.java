bb91af94c54acb59a632c9f7d6013660885961b8
sneha <sneha.ukarande@lntinfotech.com>
2021-01-13 15:42:28 +0530

    insurance module

--- src/main/java/com/lti/daos/InsuranceDAO.java
+++ src/main/java/com/lti/daos/InsuranceDAO.java
@@ -1,8 +1,11 @@
 package com.lti.daos;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public class InsuranceDAO extends GenericDAO{
+	
 
 	
 }
--- src/main/java/com/lti/services/InsuranceService.java
+++ src/main/java/com/lti/services/InsuranceService.java
@@ -20,38 +20,44 @@ public class InsuranceService implements IInsuranceService {
 	
 	@Override
 	public int registerPolicy(Insurance insurance) {
-		// TODO Auto-generated method stub
-		return 0;
+		Insurance reg =(Insurance) insuranceDAO.save(insurance);
+		
+		return reg.getInsurance_id();
 	}
 
 	@Override
 	public int registerClaim(Claim claim) {
-		// TODO Auto-generated method stub
-		return 0;
+		
+		Claim request =(Claim) insuranceDAO.save(claim);
+		
+		return request.getClaim_id();
+		
 	}
 
 	@Override
 	public Insurance getInsuranceById(int insurance_id) {
-		// TODO Auto-generated method stub
+		
 		return null;
 	}
 
 	@Override
 	public Claim getClaimById(int claim_id) {
-		// TODO Auto-generated method stub
+		
 		return null;
 	}
 
 	@Override
 	public List<Insurance> getInsurancesByUserId(int user_id) {
-		// TODO Auto-generated method stub
+
 		return null;
 	}
 
 	@Override
 	public List<Claim> getClaimsByUserId(int user_id) {
-		// TODO Auto-generated method stub
+
 		return null;
 	}
+
+
 
 }
